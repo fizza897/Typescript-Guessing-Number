b9b80820a34a3335f2479c4389e0d4713a46dce3
fizza897 <99113832+fizza897@users.noreply.github.com>
2023-01-04 01:21:27 +0500

    Add files via upload

---
+++ Guess The Number/index.ts
@@ -0,0 +1,34 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import  showBanner from"node-banner"
+let play=true
+let score=0;
+(async () => {
+    await showBanner('Guess Number', 'Guess to Win!');
+})();
+async function guessNumber(){
+    while (play){
+
+        let randomNumber=Math.ceil((Math.random()*4)+1)
+        let answer=await inquirer.prompt([{
+            name:"userNumber",
+            type:"number",
+            message:"Enter any Number 1 to 5"
+        }]);
+        if(answer.userNumber===randomNumber){
+            console.log(chalk.green("You Guess a Right Number "))
+            score +=10;
+            console.log(chalk.yellow(`Yours scores are: ${score}`))
+        }
+        else{
+            console.log(chalk.red("Try Again!!"))
+            play= false
+        }
+    }
+    }
+    setTimeout(()=>{
+
+        guessNumber()
+    },1000)
+
